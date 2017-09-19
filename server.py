58a21603c801197d6945060242209d3f4b4ac2b0
Kosy Anyanwu <kosyfrances@gmail.com>
2017-09-19 23:00:16 +0100

    Display mount points on server

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+from mount_points import get_mount_points
+
 
 class GetHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        message = 'It works'
+        message = get_mount_points()
         self.send_response(200)
-        self.send_header()
-        self.wfile.write(message)
+        self.send_header('Content-type', 'text/json')
+        self.wfile.write(bytes(message, "utf8"))
         return
 
 
